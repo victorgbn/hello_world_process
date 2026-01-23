06df45a664f81802c065442c5f8bb1209b9d1041
Victor Gaubin <vgaubin@mercator-ocean.fr>
2026-01-23 11:38:45 +0100

    add timeout

--- script.py
+++ script.py
@@ -1,7 +1,9 @@
-def print_numbers_1_to_100():
-    """Print numbers from 1 to 100 inclusive, one per line."""
-    for i in range(1, 101):
+import time
+def print_numbers_1_to_1000():
+    """Print numbers from 1 to 1000 inclusive, one per line."""
+    for i in range(1, 1001):
+        time.sleep(10)
         print(i)
 
 if __name__ == "__main__":
-    print_numbers_1_to_100()
+    print_numbers_1_to_1000()
