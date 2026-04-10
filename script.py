abf91763c2f1a853c69f5f1484737fe2f56466a7
Victor Gaubin <vgaubin@mercator-ocean.fr>
2026-04-10 11:35:56 +0200

    add print variables

--- script.py
+++ script.py
@@ -1,12 +1,27 @@
 import time
 import logging
-
+import os
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s | %(levelname)s | %(message)s",
 )
 
 logger = logging.getLogger(__name__)
+
+def print_env_varibales():
+    logger.info("Environment Variables :")
+    print("s3 values")
+    print(os.environ.get("AWS_ACCESS_KEY_ID"))
+    print(os.environ.get("AWS_SECRET_ACCESS_KEY"))
+
+    print("Copernicus Marine values :")
+    print(os.environ.get("COPERNICUSMARINE_SERVICE_USERNAME"))
+    print(os.environ.get("COPERNICUSMARINE_SERVICE_PASSWORD"))
+
+    print("Others :")
+    for key, value in os.environ.items():
+        if key not in ["AWS_ACCESS_KEY_ID", "AWS_SECRET_ACCESS_KEY", "COPERNICUSMARINE_SERVICE_USERNAME", "COPERNICUSMARINE_SERVICE_PASSWORD"]:
+            print("%s: %s", key, value)
 
 def print_numbers_1_to_1000():
     for i in range(1, 1001):
@@ -15,5 +30,6 @@ def print_numbers_1_to_1000():
 
 if __name__ == "__main__":
     logger.info("Starting counter...")
-    print_numbers_1_to_1000()
+    print_env_varibales()
+    # print_numbers_1_to_1000()
     logger.info("Counter finished.")
